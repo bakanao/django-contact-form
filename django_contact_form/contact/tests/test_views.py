66f376a8480ec3ab26b33793d645a68da8876998
OY <oy@prontomarketing.com>
2015-05-06 17:32:13 +0700

    Render Contact form header

--- django_contact_form/contact/tests/test_views.py
+++ django_contact_form/contact/tests/test_views.py
@@ -3,3 +3,8 @@ class ContactViewTest(TestCase):
     def test_contact_page_should_be_accessible(self):
         response = self.client.get('/contact/')
         self.assertEqual(response.status_code, 200)
+
+    def test_contact_page_should_have_wording_contact_form(self):
+        response = self.client.get('/contact/')
+        expected = '<h1>Contact Form</h1>'
+        self.assertContains(response, expected, status_code=200)
--- django_contact_form/contact/views.py
+++ django_contact_form/contact/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class ContactView(TemplateView):
     template_name='contact.html'
+
+    def get(self, request):
+        return render(request, self.template_name, {})
