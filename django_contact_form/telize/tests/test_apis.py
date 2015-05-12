0ee13ac8920a3ca192138382fb12f9968c2c6883
OY <oy@prontomarketing.com>
2015-05-12 13:46:35 +0700

    implement telize api to get geoip

--- django_contact_form/telize/tests/test_apis.py
+++ django_contact_form/telize/tests/test_apis.py
@@ -1,10 +1,35 @@
+from mock import patch
+
 from django.test import TestCase
 
 from telize.api.geoip import Geoip
 
 
 class GeoipTest(TestCase):
-    def test_get_geoip_should_return_correct_data(self):
+    def setUp(self):
+        self.geoip = Geoip()
+
+    @patch('telize.api.geoip.requests.get')
+    def test_get_geoip_should_return_correct_data(self, mock):
+        mock.return_value.json.return_value = {
+            u'city': u'Bangkok',
+            u'region_code': u'40',
+            u'dma_code': u'0',
+            u'ip': u'58.137.162.34',
+            u'region': u'Krung Thep',
+            u'isp': u'CS LOXINFO PUBLIC COMPANY LIMITED',
+            u'area_code': u'0',
+            u'longitude': 100.5014,
+            u'country_code3': u'THA',
+            u'continent_code': u'AS',
+            u'country_code': u'TH',
+            u'offset': u'7',
+            u'latitude': 13.754,
+            u'timezone': u'Asia/Bangkok',
+            u'country': u'Thailand',
+            u'asn': u'AS4750'
+        }
+
         expected = {
             "longitude": 100.5014,
             "latitude": 13.754,
@@ -15,7 +40,7 @@ class GeoipTest(TestCase):
             "continent_code": "AS",
             "dma_code": "0",
             "city": "Bangkok",
-            "timezone": "Asia\/Bangkok",
+            "timezone": "Asia/Bangkok",
             "region": "Krung Thep",
             "country_code": "TH",
             "isp": "CS LOXINFO PUBLIC COMPANY LIMITED",
@@ -24,6 +49,10 @@ class GeoipTest(TestCase):
             "region_code":"40"
         }
 
-        geoip = Geoip()
-        result = geoip.get()
+        result = self.geoip.get()
         self.assertDictEqual(result, expected)
+
+    @patch('telize.api.geoip.requests.get')
+    def test_get_geoip_should_call_telize_api(self, mock):
+        self.geoip.get()
+        mock.assert_called_once_with('http://www.telize.com/geoip')
