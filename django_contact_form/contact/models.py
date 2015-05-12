1cc6a49978b7b8a4661c24cafe1ffc09dea9ab1e
OY <oy@prontomarketing.com>
2015-05-12 13:47:45 +0700

    add ip and location fields to contact model

--- django_contact_form/contact/models.py
+++ django_contact_form/contact/models.py
@@ -5,3 +5,5 @@ class Contact(models.Model):
     first_name = models.CharField(null=False, blank=False, max_length=50)
     last_name = models.CharField(null=False, blank=False, max_length=50)
     email = models.CharField(null=False, blank=False, max_length=50)
+    ip = models.CharField(null=False, blank=False, max_length=50)
+    location = models.CharField(null=False, blank=False, max_length=50)
--- django_contact_form/contact/tests/test_models.py
+++ django_contact_form/contact/tests/test_models.py
@@ -10,6 +10,8 @@ class ContactTest(TestCase):
         contact.first_name = 'Navarat'
         contact.last_name = 'Pramuksun'
         contact.email = 'oy@prontomarketing.com'
+        contact.ip = '58.137.162.34'
+        contact.location = '13.754:100.5014'
 
         self.assertFalse(contact.id)
 
@@ -28,11 +30,19 @@ class ContactTest(TestCase):
         expected = 'oy@prontomarketing.com'
         self.assertEqual(contact_new.email, expected)
 
+        expected = '58.137.162.34'
+        self.assertEqual(contact_new.ip, expected)
+
+        expected = '13.754:100.5014'
+        self.assertEqual(contact_new.location, expected)
+
     def test_save_contact_without_first_name_should_fail(self):
         contact = Contact()
         contact.first_name = None
         contact.last_name = 'Pramuksun'
         contact.email = 'oy@prontomarketing.com'
+        contact.ip = '58.137.162.34'
+        contact.location = '13.754:100.5014'
 
         self.assertRaises(IntegrityError, contact.save)
 
@@ -41,6 +51,8 @@ class ContactTest(TestCase):
         contact.first_name = 'Navarat'
         contact.last_name = None
         contact.email = 'oy@prontomarketing.com'
+        contact.ip = '58.137.162.34'
+        contact.location = '13.754:100.5014'
 
         self.assertRaises(IntegrityError, contact.save)
 
@@ -49,5 +61,27 @@ class ContactTest(TestCase):
         contact.first_name = 'Navarat'
         contact.last_name = 'Pramuksun'
         contact.email = None
+        contact.ip = '58.137.162.34'
+        contact.location = '13.754:100.5014'
+
+        self.assertRaises(IntegrityError, contact.save)
+
+    def test_save_contact_without_ip_should_fail(self):
+        contact = Contact()
+        contact.first_name = 'Navarat'
+        contact.last_name = 'Pramuksun'
+        contact.email = 'oy@pronto.com'
+        contact.ip = None
+        contact.location = '13.754:100.5014'
+
+        self.assertRaises(IntegrityError, contact.save)
+
+    def test_save_contact_without_location_should_fail(self):
+        contact = Contact()
+        contact.first_name = 'Navarat'
+        contact.last_name = 'Pramuksun'
+        contact.email = 'oy@pronto.com'
+        contact.ip = '58.137.162.34'
+        contact.location = None
 
         self.assertRaises(IntegrityError, contact.save)
