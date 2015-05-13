12eaef63e10749aef52afd2da7acc7c1d10f53db
OY <oy@prontomarketing.com>
2015-05-13 22:28:30 +0700

    (forgot to add with previous commit) create comments model with name and comment

--- django_contact_form/comments/models.py
+++ django_contact_form/comments/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Comments(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=50)
+    comment = models.TextField(null=False, blank=False, max_length=50)
