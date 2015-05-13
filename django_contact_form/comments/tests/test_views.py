6d04fdfd4221d4bb982d71d7f36813737cfe612b
OY <oy@prontomarketing.com>
2015-05-13 20:33:17 +0700

    add title Comments on comments page

--- django_contact_form/comments/tests/test_views.py
+++ django_contact_form/comments/tests/test_views.py
@@ -2,6 +2,13 @@ from django.test import TestCase
 
 
 class CommentsViewTest(TestCase):
-    def test_comments_view_should_be_accessible(self):
+    def test_comments_page_should_be_accessible(self):
         response = self.client.get('/comments/')
         self.assertEqual(response.status_code, 200)
+
+    def test_comments_page_should_display_title_comments(self):
+        response = self.client.get('/comments/')
+        expected = '<h1>Comments</h1>'
+
+        self.assertContains(response, expected, status_code=200)
+
