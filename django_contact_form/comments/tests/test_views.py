baa34b808a3eea836712081260250684f8be77f1
OY <oy@prontomarketing.com>
2015-05-13 22:24:36 +0700

    create html comment form on comments page

--- django_contact_form/comments/tests/test_views.py
+++ django_contact_form/comments/tests/test_views.py
@@ -9,6 +9,18 @@ class CommentsViewTest(TestCase):
     def test_comments_page_should_display_title_comments(self):
         response = self.client.get('/comments/')
         expected = '<h1>Comments</h1>'
-
         self.assertContains(response, expected, status_code=200)
 
+    def test_comments_page_should_have_comments_form(self):
+        response = self.client.get('/comments/')
+        expected = '<form action="." method="post">'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '<td><input id="name" name="name" type="text" /></td>'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '<td><textarea id="comment" name="comment"></textarea></td>'
+        self.assertContains(response, expected, status_code=200)
+
+        expected = '<input type="submit" value="Comment!">'
+        self.assertContains(response, expected, status_code=200)
