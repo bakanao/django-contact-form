0042f921aecb30336643edfcb4cf9e60a3f3c630
OY <oy@prontomarketing.com>
2015-05-15 14:50:01 +0700

    remove field ip from comments form

--- django_contact_form/comments/forms.py
+++ django_contact_form/comments/forms.py
@@ -4,4 +4,3 @@ from django import forms
 class CommentsForm(forms.Form):
     name = forms.CharField(max_length=100, required=True)
     comment = forms.CharField(widget = forms.Textarea)
-    ip = forms.CharField(max_length=100, required=True)
--- django_contact_form/comments/tests/test_forms.py
+++ django_contact_form/comments/tests/test_forms.py
@@ -5,10 +5,10 @@ from comments.forms import CommentsForm
 
 class CommentsFormTest(TestCase):
     def test_comments_form_should_have_name_and_comment_fields(self):
-        expected_fields = ['name', 'comment', 'ip']
+        expected_fields = ['name', 'comment']
 
         form = CommentsForm()
         for each in expected_fields:
             self.assertTrue(each in form.fields)
 
-        self.assertEqual(len(form.fields), 3)
+        self.assertEqual(len(form.fields), 2)
